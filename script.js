4a60979b59a7b36b00f2917651c885f8e96ece63
ozymeric <mail.ozanmeric@gmail.com>
2022-11-20 14:13:39 +0300

    added js codes, close to finelize the project

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+let turtleDiv = document.querySelector(".content1");
+let rabbitDiv = document.querySelector(".content2");
+
+let turtleButton = document.querySelector(".content1 a");
+let rabbitButton = document.querySelector(".content2 a");
+
+function expandTurtle() {
+  turtleDiv.style.flex = 7;
+  turtleButton.classList.add("active");
+  rabbitDiv.style.flex = 3;
+  rabbitButton.classList.remove("active");
+}
+
+function expandRabbit() {
+  turtleDiv.style.flex = 3;
+  turtleButton.classList.remove("active");
+  rabbitDiv.style.flex = 7;
+  rabbitButton.classList.add("active");
+}
+
+function returnBasics() {
+  turtleDiv.style.flex = 1;
+  turtleButton.classList.remove("active");
+  rabbitDiv.style.flex = 1;
+  rabbitButton.classList.remove("active");
+}
+
+turtleDiv.addEventListener("mouseenter", expandTurtle);
+turtleDiv.addEventListener("mouseleave", returnBasics);
+
+rabbitDiv.addEventListener("mouseenter", expandRabbit);
+rabbitDiv.addEventListener("mouseleave", returnBasics);
 // ###########################################################
 // ###########################################################
 // #################### - UDEMY SOLUTION - ###################
